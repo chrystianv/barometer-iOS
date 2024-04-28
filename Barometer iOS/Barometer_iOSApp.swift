c5232fb4aaeff147390873134ff09c6ce2983213
Chrystian Vieyra <chrys.vieyra@gmail.com>
2024-04-28 13:58:15 -0400

    Initial Commit

---
+++ Barometer iOS/Barometer_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barometer_iOSApp.swift
+//  Barometer iOS
+//
+//  Created by Chrystian Vieyra on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Barometer_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barometer iOSUITests/Barometer_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Barometer_iOSUITestsLaunchTests.swift
+//  Barometer iOSUITests
+//
+//  Created by Chrystian Vieyra on 4/28/24.
+//
+
+import XCTest
+
+final class Barometer_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
